5451a3993f10778807d7202f60aaeb848b81704a
Taranajib <44290967+Taranajib@users.noreply.github.com>
2018-11-12 16:33:42 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/kku/cs/lab9_kotlin/SecondActvity.java
@@ -0,0 +1,13 @@
+package th.ac.kku.cs.lab9_kotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_actvity);
+    }
+}
